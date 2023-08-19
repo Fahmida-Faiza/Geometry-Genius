3dfa10cb13040e28763d8cf229de8e70f1e7ff86
Fahmida Faiza <fahmidafaiza57@gmail.com>
2023-08-19 21:26:13 +0600

    dynamic add to calculation entry

--- scripts/area-calculator.js
+++ scripts/area-calculator.js
@@ -1,24 +1,26 @@
 function calculateTriangleArea(){
-    //get triangle base value
-    const baseField = document.getElementById("triangle-base");
-    const baseValueText = baseField.value;
-    const base = parseFloat(baseValueText);
+  //get triangle base value
+  const baseField = document.getElementById("triangle-base");
+  const baseValueText = baseField.value;
+  const base = parseFloat(baseValueText);
 
+  //get triangle height value
+  const heightField = document.getElementById("triangle-height");
+  const heightValueText = heightField.value;
+  const height = parseFloat(heightValueText);
 
-    //get triangle height value
-    const heightField = document.getElementById("triangle-height");
-    const heightValueText = heightField.value;
-    const height = parseFloat(heightValueText);
+  const area = 0.5 * base * height;
+  console.log(area);
 
+  //show area triangle
 
-    const area = 0.5 * base *height;
-    console.log(area)
-
-    //show area triangle
-
-    const areaSpan = document.getElementById("triangle-area");
-    areaSpan.innerText = area;
+  const areaSpan = document.getElementById("triangle-area");
+  areaSpan.innerText = area;
+//
+  addToCalculationEntry("triangle", area);
 }
+
+
 
 //reusable code added function in rectangle and paraligram
 function calculateRectangleArea(){
@@ -30,6 +32,9 @@ function calculateRectangleArea(){
  //area calculate
  const area = width * length;
  setElementInnerText("reactangle-area" , area);
+
+ //add to calculation entry
+  addToCalculationEntry("rectangle", area);
 }
 //paralaligram
 function calculateParallelogramArea(){
@@ -41,6 +46,11 @@ function calculateParallelogramArea(){
     //calculate area
     const area =base * height;
     setElementInnerText("parallelogram-area" ,area);
+
+
+
+    //add to calculation entry
+    addToCalculationEntry('Parallelogram' ,area);
 }
 
 // get input value feild in number
@@ -55,4 +65,26 @@ function getInputValue(fieldId){
 function setElementInnerText(elementId , area){
     const element = document.getElementById(elementId)
     element.innerText = area;
+}
+
+
+
+
+
+
+//add to calculation entry
+// 1.get the element 
+//2. create an element you want to add
+//3.set innerhtml  it will bedynamic template string 
+function addToCalculationEntry(areaType ,area){
+    console.log('addimg soon')
+    const calculationEntry = document.getElementById("calculation-entry");
+//dynamic 1,2,3
+const count = calculationEntry.childElementCount;
+
+    const p =document.createElement('p');
+    p.classList.add('my-4');//margin gap 
+    // p.innerHTML = areaType + ' ' +  area;
+    p.innerHTML = `${count + 1}. ${areaType}  ${area} cm<sup>2</sup> <button class="btn btn-success">convert</button>`;
+    calculationEntry.appendChild(p);
 }
